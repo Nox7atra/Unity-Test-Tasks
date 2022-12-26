a9adeb8bee51fb8a9fc22e7e62f653d11380ca1f
Nox7atra <warsong94@gmail.com>
2022-12-26 20:42:54 +0300

    Task 1

---
+++ Assets/Unity-Test-Tasks/Task_1/Scripts/Task1_ThrowRigidBody.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class Task1_ThrowRigidBody : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private Vector2 _force;
+
+    private bool _isFlying;
+    private Vector2 _startPos;
+    private IEnumerator Start()
+    {
+        _startPos = transform.position;
+        yield return new WaitForSeconds(1f);
+        SendCircle();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SendCircle();
+        }
+    }
+
+    private void SendCircle()
+    {
+        if(_isFlying) return;
+        _isFlying = true;
+        _rigidbody2D.AddForce(_force, ForceMode2D.Impulse);
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        _isFlying = false;
+        _rigidbody2D.velocity = Vector3.zero;
+        _rigidbody2D.angularVelocity = 0;
+        if (col.transform.CompareTag("Finish"))
+        {
+            Debug.Log("Puzzle Solved");
+        }
+        else
+        {
+            Debug.Log("Try again");
+            transform.position = _startPos;
+        }
+    }
+}
